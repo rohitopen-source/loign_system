5692c8eeb41da59e346dc509629cb8c1ee7d7503
rohitopen-source <rohit931999@gmail.com>
2020-05-10 02:50:39 +0530

    port changed

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const session = require('express-session')
 const passport = require('./passport')
 
+
+const PORT = process.env.PORT || 9876
 const app = express()
 
 app.set("view engine", "hbs")
@@ -20,4 +22,4 @@ app.use('/public',require('./routes/public'))
 app.use('/private',require('./routes/private'))
 app.use('/',require('./routes/root'))
 
-app.listen(9876,()=> console.log("server running on http://localhost:9876"))
+app.listen(PORT,()=> console.log("server running on http://localhost:9876"))
